97cab8ba508472a3b038ee03a9f1fcb0d0a1b71a
Tommaso Capelli <t.capelli@avanade.com>
2019-01-30 20:13:28 +0100

    First version

---
+++ BaseCode.js
@@ -0,0 +1,11 @@
+elements = document.getElementsByClassName('pbi-frame');
+element = elements[0];
+if(element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if(element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if(element.webkitRequestFullscreen) {
+    element.webkitRequestFullscreen();
+  } else if(element.msRequestFullscreen) {
+    element.msRequestFullscreen();
+}
